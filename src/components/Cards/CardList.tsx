a5f3e13f24d04cd5e1108fee15e35db8204a8ef6
ttaerrim <ltr0121@naver.com>
2022-02-08 02:37:02 +0900

    Modify: CardList 조건부 렌더링

--- src/components/Cards/CardList.tsx
+++ src/components/Cards/CardList.tsx
@@ -2,11 +2,13 @@ import Card from 'components/Cards/Card';
 import { RequestsArray } from 'utils/types';
 const CardList: React.FC<{ data: RequestsArray }> = ({ data }) => {
     return (
-        <>
-            {data.map((item) => (
-                <Card key={item.id} item={item} />
-            ))}
-        </>
+        <div style={{ display: 'flex' }}>
+            {data.length ? (
+                data.map((item) => <Card key={item.id} item={item} />)
+            ) : (
+                <div>조건에 맞는 견적 요청이 없습니다.</div>
+            )}
+        </div>
     );
 };
 
