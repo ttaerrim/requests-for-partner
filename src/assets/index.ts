a98113f850ccc73b351f7424b5606413b3e06df8
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-08 04:08:16 +0900

    Modify: header작업을 위한 이미지파일 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 import logoIcon from 'assets/logoIcon.png';
+import logoIconColor from 'assets/logoIconColor.png';
 import companyIcon from 'assets/companyIcon.png';
+import hamburger from 'assets/hamburger.png';
 
-export { logoIcon, companyIcon };
+export { logoIcon, companyIcon, hamburger, logoIconColor };
